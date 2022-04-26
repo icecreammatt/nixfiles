777900805291682b9cc133b5a1e9f65f6c3ea118
Matt Carrier <icecreammatt@gmail.com>
2022-04-26 01:00:10 -0700

    fix: highlight colors for git

--- nvim.nix
+++ nvim.nix
@@ -127,6 +127,10 @@
             "     set termguicolors
             " endif
 
+            highlight GitGutterAdd    guifg=#009900 ctermfg=2
+            highlight GitGutterChange guifg=#bbbb00 ctermfg=3
+            highlight GitGutterDelete guifg=#ff2222 ctermfg=1
+
             syntax enable
 
             " colorscheme evening 
