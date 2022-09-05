7b3c9db6f0abf56be72b0f732b33828fdca3582c
Matt Carrier <icecreammatt@gmail.com>
2022-09-05 14:34:50 -0700

    fix: turn off line movement with esc key

--- nvim.nix
+++ nvim.nix
@@ -143,8 +143,8 @@
             " move line or visually selected block - alt+j/k
             " inoremap <A-j> <Esc>:m .+1<CR>==gi
             " inoremap <A-k> <Esc>:m .-2<CR>==gi
-            vnoremap <A-j> :m '>+1<CR>gv=gv
-            vnoremap <A-k> :m '<-2<CR>gv=gv" move split panes to left/bottom/top/right
+            " vnoremap <A-j> :m '>+1<CR>gv=gv
+            " vnoremap <A-k> :m '<-2<CR>gv=gv " move split panes to left/bottom/top/right
             nnoremap <A-h> <C-W>H
             nnoremap <A-j> <C-W>J
             nnoremap <A-k> <C-W>K
