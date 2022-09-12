ed30df2aac4727463a65279362c6270e80fae9b1
Matt Carrier <icecreammatt@gmail.com>
2022-09-12 00:16:54 -0700

    feat: undercurl support

--- tmux.nix
+++ tmux.nix
@@ -25,6 +25,12 @@
 
             set-option -g status-position top
 
+            # Undercurl
+            # set -g default-terminal "{TERM}"
+            set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
+            set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+
+
             # set aggressive resizing for mixed display sizes and shared sessions
             setw -gq aggressive-resize
 
