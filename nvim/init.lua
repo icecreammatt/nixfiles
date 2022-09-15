98f003f66b5ce7b24ea755498e0305c3e3cb7d3a
Matt Carrier <icecreammatt@gmail.com>
2022-09-14 23:25:16 -0700

    feat: add bufferline plugin settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,52 @@ local g = vim.g
 
 vim.o.completeopt = "menuone,noselect"
 
+-- bufferline settings
+require("bufferline").setup{
+    options = {
+        indicator = {
+            style = 'underline'
+        },
+        termguicolors = true,
+        diagnostics = "nvim_lsp",
+        separator_style = {"", ""},
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        diagnostics_indicator = function(count, level)
+            local icon = level:match("error") and " " or ""
+            return " " .. icon .. count
+        end,
+        -- custom_areas = {
+        --     right = function()
+        --         local result = {}
+        --         local seve = vim.diagnostic.severity
+        --         local error = #vim.diagnostic.get(0, {severity = seve.ERROR})
+        --         local warning = #vim.diagnostic.get(0, {severity = seve.WARN})
+        --         local info = #vim.diagnostic.get(0, {severity = seve.INFO})
+        --         local hint = #vim.diagnostic.get(0, {severity = seve.HINT})
+
+        --         if error ~= 0 then
+        --             table.insert(result, {text = "  " .. error, fg = "#EC5241"})
+        --         end
+
+        --         if warning ~= 0 then
+        --             table.insert(result, {text = "  " .. warning, fg = "#EFB839"})
+        --         end
+
+        --         if hint ~= 0 then
+        --             table.insert(result, {text = "  " .. hint, fg = "#A3BA5E"})
+        --         end
+
+        --         if info ~= 0 then
+        --             table.insert(result, {text = "  " .. info, fg = "#7EA9A7"})
+        --         end
+        --         return result
+        --     end
+        -- }
+    }
+}
+
 require('nvim-treesitter.configs').setup {
   highlight = {
       enable = true,
