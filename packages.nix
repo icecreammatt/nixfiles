be3d640d148a90f688a15dc98055b8d5755c3ef2
Matt Carrier <icecreammatt@gmail.com>
2022-09-16 23:55:31 -0700

    fix: move packages into its own file

---
+++ packages.nix
@@ -0,0 +1,58 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.ripgrep
+    pkgs.bat
+    pkgs.fd
+    pkgs.exa
+    pkgs.fzf
+    pkgs.coreutils
+    pkgs.jq
+    pkgs.tig
+    pkgs.gron
+    pkgs.gh
+    pkgs.delta
+    pkgs.wget
+    pkgs.tree
+    pkgs.curl
+    pkgs.bash
+    pkgs.rsync
+    pkgs.tig
+    pkgs.git
+    pkgs.ffmpeg
+    pkgs.coreutils
+    pkgs.mkcert
+    pkgs.git-standup
+    pkgs.imagemagick
+    pkgs.gifsicle
+    pkgs.gist
+    pkgs.s3cmd
+    pkgs.reattach-to-user-namespace
+    pkgs.nnn
+    pkgs.navi
+    pkgs.gitui
+    pkgs.tldr
+    pkgs.viu
+    pkgs.t-rec
+    pkgs.neofetch
+    pkgs.htop
+    pkgs.rnix-lsp
+    pkgs.time
+    pkgs.gettext
+    pkgs.hugo
+    pkgs.sumneko-lua-language-server
+    # pkgs.svelte-language-server
+    #pkgs.peek #- issue on darwin
+    #pkgs.avrdude #- issue on m1
+    #pkgs.qmk #- issue on m1
+  ];
+
+  imports = [
+    ./git.nix
+    ./fish.nix
+    ./nvim.nix
+    ./tmux.nix
+    ./gitui.nix
+  ];
+}
