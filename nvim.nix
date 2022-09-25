7fc46b6cccb1a4e50d1c0e096c8928de2d60f761
Matt Carrier <icecreammatt@gmail.com>
2022-09-25 09:47:56 -0700

    feat: try using tabs instead of spaces for accessibility

--- nvim.nix
+++ nvim.nix
@@ -121,15 +121,17 @@
           set mouse=v                 " middle-click paste with 
           set hlsearch                " highlight search 
           set incsearch               " incremental search
-          set expandtab               " converts tabs to white space
-          set shiftwidth=4            " width for autoindents
+          " set expandtab               " converts tabs to white space
+          set noexpandtab
+          set tabstop=4 " enable tabs
+          set shiftwidth=0            " width for autoindents
           set autoindent              " indent a new line the same amount as the line just typed
           " set number                  " add line numbers
           set wildmode=longest,list   " get bash-like tab completions
           " set cc=80                  " set an 80 column border for good coding style
-          filetype plugin indent on   "allow auto-indenting depending on file type
+          " filetype plugin indent on   "allow auto-indenting depending on file type
           " set mouse=a                 " enable mouse click
-          filetype plugin on
+          " filetype plugin on
 
           set ttyfast                 " Speed up scrolling in Vim
           " set spell                 " enable spell check (may need to download language package)
