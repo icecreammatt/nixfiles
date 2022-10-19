0cf450fe79b6660471b3977a13711427067490a1
Matt Carrier <icecreammatt@gmail.com>
2022-10-19 14:16:27 -0700

    feat: add more aggressive branch clean command

--- fish.nix
+++ fish.nix
@@ -57,6 +57,7 @@
         gsl = "git stash list";
         grh = "git reset --hard";
         gclean = "git remote prune origin; git branch --merged | grep -v -E \"(\*|master|main|dev|release|hotfix|trunk|prod)\" | xargs -n 1 git branch -d";
+        gcleanall = "git remote prune origin; git branch | grep -v -E \"(\*|master|main|dev|release|hotfix|trunk|prod)\" | xargs -n 1 git branch -D";
         gs = "git show";
         gf = "git fetch --all";
         gl = "git log";
