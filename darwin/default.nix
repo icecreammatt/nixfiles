b4004b1e2f0f2768c3cf2b0007049d0fc26c75ff
Matt Carrier <icecreammatt@gmail.com>
2022-12-03 22:29:19 -0800

    fix: use hosts config

--- darwin/default.nix
+++ darwin/default.nix
@@ -7,11 +7,10 @@ in
 {
   mc-2A3MD6R-MBP = darwin.lib.darwinSystem {
     system = "x86_64-darwin";
-
     specialArgs = { inherit user inputs; };
     modules = [
-      ./configuration.nix
-
+      #./configuration.nix
+      ./hosts/mc-2A3MD6R-MBP/default.nix
       home-manager.darwinModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
