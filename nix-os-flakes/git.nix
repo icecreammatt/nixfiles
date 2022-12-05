95f341e8144159f3eb8d07cbaf351fb9da914a50
Matt Carrier <icecreammatt@gmail.com>
2022-12-04 16:44:44 -0800

    fix: hard code username for now

--- nix-os-flakes/git.nix
+++ nix-os-flakes/git.nix
@@ -1,8 +1,8 @@
 {
 programs.git = {
     enable = true;
-    userName = (builtins.fromJSON(builtins.readFile ./config.json)).fullName;
-    userEmail = (builtins.fromJSON(builtins.readFile ./config.json)).email;
+    userName = "Matt Carrier";
+    #userEmail = (builtins.fromJSON(builtins.readFile ./config.json)).email;
     aliases = {
         lg1 = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
         lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
