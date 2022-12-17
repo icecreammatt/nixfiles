d4a36b7e721b6e84c52dedd8c1ecfe6c9422df3e
Matt Carrier <icecreammatt@gmail.com>
2022-12-17 01:51:02 -0800

    feat: add killall command

--- nix-os-flakes/configuration.nix
+++ nix-os-flakes/configuration.nix
@@ -138,6 +138,7 @@ nixpkgs.config.packageOverrides = pkgs: {
 
   environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     killall
   #  wget
   ];
 
