c27d3dc5a8c706c32b0b9bee333a5551256468a8
Matt Carrier <icecreammatt@gmail.com>
2022-12-18 17:31:51 -0800

    feat: styling for kitty

--- modules/shell/kitty.nix
+++ modules/shell/kitty.nix
@@ -2,5 +2,6 @@
 
 {
   home.file.".config/kitty/kitty.conf".source = ./kitty/kitty.conf;
+  home.file.".config/kitty/tab_bar.py".source = ./kitty/tab_bar.py;
   home.file.".config/kitty/dracula.conf".source = ./kitty/dracula.conf;
 }
