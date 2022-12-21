00bf71e012eb32eac2e93ff996e983fee6bee8cf
Matt Carrier <icecreammatt@gmail.com>
2022-12-21 11:35:55 -0800

    style: whitespace

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -139,25 +139,26 @@
         ag = "rg";
     };
     functions = {
-	ranger = {
-		body = ''
+    ranger = {
+        body = ''
 
-		set tempfile (mktemp -t tmp.XXXXXX)
-		command ranger --choosedir=$tempfile $argv
-		set return_value $status
+        set tempfile (mktemp -t tmp.XXXXXX)
+        command ranger --choosedir=$tempfile $argv
+        set return_value $status
 
-		if test -s $tempfile
-			set ranger_pwd (cat $tempfile)
-			if test -n $ranger_pwd -a -d $ranger_pwd
-				builtin cd -- $ranger_pwd
-			end
-		end
+        if test -s $tempfile
+            set ranger_pwd (cat $tempfile)
+            if test -n $ranger_pwd -a -d $ranger_pwd
+                builtin cd -- $ranger_pwd
+            end
+        end
 
-		command rm -f -- $tempfile
-		return $return_value
-	'';
+        command rm -f -- $tempfile
+        return $return_value
 
-	};
+    '';
+
+    };
         fish_greeting = {
           description = "welcome message";
           body = "";
