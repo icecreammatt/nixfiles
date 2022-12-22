9757966d00ab1aaff609aa77eb12567f451e7a30
Matt Carrier <icecreammatt@gmail.com>
2022-12-21 18:20:29 -0800

    feat: pin to node 16.13.1 for x86 darwin

--- darwin/hosts/mc-2A3MD6R-MBP/default.nix
+++ darwin/hosts/mc-2A3MD6R-MBP/default.nix
@@ -1,6 +1,16 @@
 # hosts/mc-2A3MD6R-MBP/default.nix
 { pkgs, ... }:
 
+let
+    system = "x86_64-darwin"; # Todo see how to pass this through from parent
+    # version = "16.13.1"; <-- this is for refence for the build sha below
+    pkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
+      sha256 = "13s8g6p0gzpa1q6mwc2fj2v451dsars67m4mwciimgfwhdlxx0bk";
+    }) { inherit system; };
+
+    node16 = pkgs.nodejs;
+in
 {
   nix.extraOptions = ''
     experimental-features = nix-command flakes
@@ -31,6 +41,7 @@
     home.packages = with pkgs; [
       reattach-to-user-namespace
       home-manager
+      node16
     ];
 
     home.stateVersion = "22.11";
