4933108ebab747064e46b0f87441c59bcbeefcbc
Matt Carrier <icecreammatt@gmail.com>
2023-01-14 17:12:02 -0800

    feat: add settings for helix

--- modules/editors/helix.nix
+++ modules/editors/helix.nix
@@ -5,5 +5,31 @@
 
   home.file.".config/helix/config.toml".text = ''
     theme = "dracula"
+
+    [editor]
+    scrolloff = 9999999999
+    cursorline = true
+    bufferline = "multiple"
+
+    [editor.cursor-shape]
+    insert = "bar"
+    select = "underline"
+
+    [editor.whitespace.render]
+    space = "none"
+    tab = "all"
+    newline = "all"
+
+    [editor.whitespace.characters]
+    space = "·"
+    nbsp = "⍽"
+    tab = "→"
+    newline = "⏎"
+    tabpad = "·" # Tabs will look like "→···" (depending on tab width)
+
+    [editor.indent-guides]
+    render = true
+    character = "⸽"
+    skip-levels = 1
   '';
 }
