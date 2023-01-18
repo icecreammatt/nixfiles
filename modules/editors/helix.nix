4220e46c647ad19818e6ed43f61fae5d21c7a015
Matt Carrier <icecreammatt@gmail.com>
2023-01-17 20:57:33 -0800

    feat: vim colemak hybrid

--- modules/editors/helix.nix
+++ modules/editors/helix.nix
@@ -31,5 +31,48 @@
     render = true
     character = "⸽"
     skip-levels = 1
+
+    [keys.insert]
+    "," = { s = "normal_mode" }
+
+    [keys.normal]
+    # -------------------------
+    # QWERTY to Colemak remaps
+    # [JY] [LU] [UI] [..]
+    #  [..] [NJ] [EK] [IL]
+    #   [KN] [..] [..] [..]
+    # -------------------------
+
+    # N <=> K (swap, QWERTY position)
+    e = "move_line_down"
+    E = "keep_selections"
+    k = "search_next"
+    K = "search_prev"
+
+    # E <=> J (swap actions)
+    j = "move_next_word_end"
+    J = "move_next_long_word_end"
+
+    u = "move_line_up"
+    U = "join_selections"
+
+    # ILU loop
+    # I => L
+    i = "move_char_right"
+    I = "insert_at_line_end"
+
+    n = "move_char_left"
+    N = "workspace_symbol_picker"
+
+    s = "insert_mode"
+    S = "insert_at_line_start"
+    h = "select_regex"
+    H = "split_selection"
+    "A-h" = "split_selection_on_newline"
+
+    # U => I (QWERTY position)
+    # L => U (QWERTY position)
+    l = "undo"
+    L = "redo"
   '';
 }
