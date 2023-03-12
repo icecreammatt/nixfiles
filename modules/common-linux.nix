2a3039272dd0ba5a7cd0da5e3f70d7aab1ee9628
Matt Carrier <icecreammatt@gmail.com>
2023-03-12 14:30:17 -0700

    fix: move gui apps out of common-linux

--- modules/common-linux.nix
+++ modules/common-linux.nix
@@ -3,22 +3,8 @@
 {
   home.packages = with pkgs; [
     bmon
-    qmk
-    avrdude
-
-    vscode-extensions.vadimcn.vscode-lldb
-
-    font-awesome
-    nerdfonts
-
-    cava
-    freerdp
-    hyprpaper
-    krakenx
     lm_sensors
     psensor
-    vscode
-    peek
     #nvtop #nonfree
   ];
 }
