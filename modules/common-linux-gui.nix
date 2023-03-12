efd0678912b2bdb07d1ea8d8b091a170b29078da
Matt Carrier <icecreammatt@gmail.com>
2023-03-12 14:30:03 -0700

    feat: add common-linux-gui

---
+++ modules/common-linux-gui.nix
@@ -0,0 +1,27 @@
+{  pkgs, lib, ... }:
+
+{
+  home.packages = with pkgs; [
+    xclip # xwindow clipboard
+    bmon
+
+    vscode-extensions.vadimcn.vscode-lldb
+
+    font-awesome
+    nerdfonts
+
+    cava
+    freerdp
+    hyprpaper
+    krakenx
+    lm_sensors
+    psensor
+    vscode
+    peek
+    #nvtop #nonfree
+  ];
+
+  imports = [
+    ./shell/cava.nix
+  ];
+}
