a319e275a319dbc7ab9967c477bf3ce597725788
Matt Carrier <icecreammatt@gmail.com>
2023-03-15 21:31:14 -0700

    docs: add comments

--- flake.nix
+++ flake.nix
@@ -1,23 +1,26 @@
 # This file is the entrypoint into all the system configurations
 
 {
-  description = "System configuration for NixOS, Mac, Asahi";
+  description = "System configuration for NixOS, Mac, Asahi, RaspberryPi, NixOS VMs";
 
   inputs = { 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
+    # Configuration and Program Management
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Mac System Configuration
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Linux Desktop Window Manager
     hyprland = {
       url = "github:vaxerski/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
