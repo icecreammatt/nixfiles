629605ea64317ed9b80276d81b107ee2ddb45f9d
Matt Carrier <icecreammatt@gmail.com>
2023-03-27 00:33:51 -0700

    fix: add hostPlatform to fix build error

--- hosts/nixos/pi4/hardware-configuration.nix
+++ hosts/nixos/pi4/hardware-configuration.nix
@@ -13,6 +13,8 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
