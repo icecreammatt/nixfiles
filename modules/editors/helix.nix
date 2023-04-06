04680b572421141ca619fc537e35bb03a0006006
Matt Carrier <icecreammatt@gmail.com>
2023-04-05 17:14:50 -0700

    feat: add C-r as alternative to open picker

--- modules/editors/helix.nix
+++ modules/editors/helix.nix
@@ -98,6 +98,7 @@
     "," = { d = ['select_mode', 'move_prev_word_start', 'move_next_word_end', 'search_selection', 'extend_search_next', 'exit_select_mode'], D = ['select_mode', 'move_prev_word_start', 'move_next_word_end', 'search_selection', 'extend_search_prev', 'exit_select_mode'],  Q = "wclose", q = ":buffer-close", w = ":w", W = ":write-quit", g = ":run-shell-command lazygit", r = ":theme catppuccin_latte", R = ":theme dracula", m = ":lsp-workspace-command", p = "file_picker_in_current_directory", "," = ["collapse_selection", "keep_primary_selection"]  }
 
     "C-p" = "file_picker_in_current_directory"
+    "C-r" = "file_picker_in_current_directory"
     "C-l" = "page_up"
 
     "C-n" = "jump_view_left"
