13853fdae64054bf8f4601e85eed171efc2f1ecf
Matt Carrier <icecreammatt@gmail.com>
2023-05-03 21:42:53 -0700

    refactor: move caddy config into separate file

---
+++ hosts/nixos/mini/caddy.nix
@@ -0,0 +1,116 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    caddy
+  ];
+
+  services.caddy = {
+    enable = true;
+
+    extraConfig = ''
+        :80 {
+          encode gzip
+          file_server
+
+          handle_path /* {
+            root * "/mnt/storage/webroot"
+            file_server browse
+          }
+        }
+    '';
+
+    virtualHosts."mini.dev.c4er.com".extraConfig = ''
+        encode gzip
+        file_server
+        tls /mnt/certs/dev.c4er.com/fullchain1.pem  /mnt/certs/dev.c4er.com/privkey1.pem
+
+        handle_path /media/* {
+          root * "/mnt/storage/rewind"
+          file_server browse
+        }
+
+        handle_path /* {
+          reverse_proxy localhost:5173
+        }
+    '';
+
+    virtualHosts."dev.rewind.c4er.com".extraConfig = ''
+        encode gzip
+        file_server
+        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
+
+        handle_path /media/* {
+          root * "/mnt/storage/rewind"
+          file_server browse
+        }
+
+        handle_path /* {
+          reverse_proxy localhost:5173
+        }
+    '';
+
+    virtualHosts."rewind.c4er.com".extraConfig = ''
+        encode gzip
+        file_server
+        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
+
+        handle_path /media/* {
+          root * "/mnt/storage/rewind"
+          file_server browse
+        }
+
+        handle_path /* {
+          reverse_proxy localhost:5173
+        }
+    '';
+
+    virtualHosts."storybook.rewind.c4er.com".extraConfig = ''
+        encode gzip
+        file_server
+        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
+
+        handle_path /media/* {
+          root * "/mnt/storage/rewind"
+          file_server browse
+        }
+
+        handle_path /* {
+          reverse_proxy localhost:6006
+        }
+    '';
+
+    virtualHosts."mini-vpn.dev.c4er.com".extraConfig = ''
+        encode gzip
+        file_server
+        tls /mnt/certs/dev.c4er.com/fullchain1.pem  /mnt/certs/dev.c4er.com/privkey1.pem
+
+        handle_path /media/* {
+          root * "/mnt/storage/rewind"
+          file_server browse
+        }
+
+        handle_path /* {
+          reverse_proxy localhost:5173
+        }
+    '';
+
+    virtualHosts."bw-vpn.dev.c4er.com".extraConfig = ''
+        tls /mnt/certs/dev.c4er.com/fullchain1.pem  /mnt/certs/dev.c4er.com/privkey1.pem
+
+        handle_path /* {
+          reverse_proxy localhost:8000
+        }
+    '';
+
+    virtualHosts."pocketbase.rewind.c4er.com".extraConfig = ''
+        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
+
+        handle_path /* {
+          reverse_proxy localhost:8090
+        }
+    '';
+    
+  };
+
+}
--- hosts/nixos/mini/configuration.nix
+++ hosts/nixos/mini/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./caddy.nix
     ];
 
   services.vaultwarden.enable = true;
@@ -16,114 +17,6 @@
   # services.k3s.extraFlags = toString [
     # "--kubelet-arg=v=4" # Optionally add additional args to k3s
   # ];
-
-  services.caddy = {
-    enable = true;
-
-    extraConfig = ''
-        :80 {
-          encode gzip
-          file_server
-
-          handle_path /* {
-            root * "/mnt/storage/webroot"
-            file_server browse
-          }
-        }
-    '';
-
-    virtualHosts."mini.dev.c4er.com".extraConfig = ''
-        encode gzip
-        file_server
-        tls /mnt/certs/dev.c4er.com/fullchain1.pem  /mnt/certs/dev.c4er.com/privkey1.pem
-
-        handle_path /media/* {
-          root * "/mnt/storage/rewind"
-          file_server browse
-        }
-
-        handle_path /* {
-          reverse_proxy localhost:5173
-        }
-    '';
-
-    virtualHosts."dev.rewind.c4er.com".extraConfig = ''
-        encode gzip
-        file_server
-        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
-
-        handle_path /media/* {
-          root * "/mnt/storage/rewind"
-          file_server browse
-        }
-
-        handle_path /* {
-          reverse_proxy localhost:5173
-        }
-    '';
-
-    virtualHosts."rewind.c4er.com".extraConfig = ''
-        encode gzip
-        file_server
-        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
-
-        handle_path /media/* {
-          root * "/mnt/storage/rewind"
-          file_server browse
-        }
-
-        handle_path /* {
-          reverse_proxy localhost:5173
-        }
-    '';
-
-    virtualHosts."storybook.rewind.c4er.com".extraConfig = ''
-        encode gzip
-        file_server
-        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
-
-        handle_path /media/* {
-          root * "/mnt/storage/rewind"
-          file_server browse
-        }
-
-        handle_path /* {
-          reverse_proxy localhost:6006
-        }
-    '';
-
-    virtualHosts."mini-vpn.dev.c4er.com".extraConfig = ''
-        encode gzip
-        file_server
-        tls /mnt/certs/dev.c4er.com/fullchain1.pem  /mnt/certs/dev.c4er.com/privkey1.pem
-
-        handle_path /media/* {
-          root * "/mnt/storage/rewind"
-          file_server browse
-        }
-
-        handle_path /* {
-          reverse_proxy localhost:5173
-        }
-    '';
-
-    virtualHosts."bw-vpn.dev.c4er.com".extraConfig = ''
-        tls /mnt/certs/dev.c4er.com/fullchain1.pem  /mnt/certs/dev.c4er.com/privkey1.pem
-
-        handle_path /* {
-          reverse_proxy localhost:8000
-        }
-    '';
-
-    virtualHosts."pocketbase.rewind.c4er.com".extraConfig = ''
-        tls /mnt/certs/rewind.c4er.com/fullchain1.pem  /mnt/certs/rewind.c4er.com/privkey1.pem
-
-        handle_path /* {
-          reverse_proxy localhost:8090
-        }
-    '';
-    
-  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.grub.device= "nodev";
@@ -175,7 +68,6 @@
     nebula
     morph
     tmux
-    caddy
     nodejs
     certbot-full
   ];
