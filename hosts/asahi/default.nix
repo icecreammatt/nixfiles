6b4c2cbb03e30fbc56a378ed59efa47c028f6c0e
Matt Carrier <icecreammatt@gmail.com>
2023-05-24 16:55:57 -0700

    fix: updates to asahi install

--- hosts/asahi/default.nix
+++ hosts/asahi/default.nix
@@ -1,43 +1,55 @@
 { lib, inputs, nixpkgs, home-manager, ... }:
 
 let
-  system = "aarch64-linux";
-  pkgs = nixpkgs.legacyPackages.${system};
-  overlays = [ 
-    (import ../../overlay/overlay.nix)
-  ]; 
   user = "matt";
   userName = "matt";
+  pkgs = import nixpkgs {
+    system = "aarch64-linux";
+    config.allowUnfree = true;
+    overlays = [ 
+      (import ../../overlay/overlay.nix)
+    ]; 
+  };
 in
 {
   # M1 Macbook Pro + Asahi Linux Configuration
   asahi = home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-    extraSpecialArgs = { inherit inputs user; };
+    pkgs = import nixpkgs {
+      system = "aarch64-linux";
+      config.allowUnfree = true;
+      overlays = [ 
+        (import ../../overlay/overlay.nix)
+      ]; 
+    };
+    extraSpecialArgs = { inherit inputs user pkgs; };
     modules = [
         ../../modules/common.nix
         ../../modules/shell/gitui.nix
         ../../modules/shell/tmux.nix
         ../../modules/editors/nvim.nix
-        ../../modules/DE/hypr.nix
-        ../../modules/DE/waybar.nix
-        ../../modules/DE/rofi.nix
         ../../modules/shell/kitty.nix
+        # ../../modules/DE/hypr.nix
+        # ../../modules/DE/waybar.nix
+        # ../../modules/DE/rofi.nix
         #../../modules/shell/git.nix
       {
         home = {
           username = "${userName}";
           homeDirectory = "/home/${userName}";
-          packages = [ 
-            pkgs.home-manager 
+          packages = with pkgs; [ 
             pkgs.avrdude 
-            pkgs.qmk
-            pkgs.wlr-randr
-            pkgs.nmap
+            pkgs.cascadia-code
             pkgs.dolphin
-            pkgs.rofi-wayland
-            pkgs.waybar
+            pkgs.hex2color
+            pkgs.home-manager 
+            pkgs.nerdfonts
+            pkgs.nmap
+            pkgs.qmk
+            pkgs.wezterm
             pkgs.which
+            # pkgs.rofi-wayland
+            # pkgs.waybar
+            # pkgs.wlr-randr
             # signal-desktop
           ];
           stateVersion = "22.11";
