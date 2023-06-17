d9d66811e432bc84f2bc0b0f986cf60282be2a9f
Matt Carrier <icecreammatt@gmail.com>
2023-06-17 12:23:46 -0700

    feat: add uxplay

---
+++ modules/airplay/uxplay.nix
@@ -0,0 +1,16 @@
+{
+  networking.firewall.allowedTCPPorts = [ 7000 7001 7100 ];
+  networking.firewall.allowedUDPPorts = [ 5353 6000 6001 7011 ];
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true; # printing
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      userServices = true;
+      domain = true;
+    };
+  };
+}
