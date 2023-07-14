9b581c72521ce6cc8dda51b51c6122609f8d448f
Matt Carrier <icecreammatt@gmail.com>
2023-07-13 23:56:32 -0700

    fix: remove old homebrew reference code

--- hosts/darwin/hosts/mc-2A3MD6R-MBP/default.nix
+++ hosts/darwin/hosts/mc-2A3MD6R-MBP/default.nix
@@ -41,14 +41,4 @@
     home.stateVersion = "22.11";
   };
 
-  #homebrew = {
-  #  enable = true;
-  #  autoUpdate = true;
-  # updates homebrew packages on activation,
-  # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
-  #  casks = [
-  #    "iina"
-  #  ];
-  #};
-
 }
