7633e8200d088b6c86993c2ef520d07d5fad317b
Matt Carrier <icecreammatt@gmail.com>
2023-12-12 23:16:27 -0800

    feat: add waypipe for remote application viewing

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -221,6 +221,7 @@ in
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      killall
      fish
+     waypipe
 
      # Wine for Ableton (Using Steam Proton instead)
      # wine64
