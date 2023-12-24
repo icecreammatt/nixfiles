00765b7a7ae5ea9015def111c8ce79f3645c7c4a
Matt Carrier <icecreammatt@gmail.com>
2023-12-24 11:13:47 -0800

    feat: add kde plasma to mac mini for remote desktop of signal

--- hosts/nixos/mini/configuration.nix
+++ hosts/nixos/mini/configuration.nix
@@ -15,7 +15,28 @@
   users.extraGroups.docker.members = [ "matt" ];
   users.users.matt.extraGroups = [ "docker" ];
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  programs.xwayland.enable = true;
 
+  # Enable login manager
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.settings.Wayland.SessionDir = "${pkgs.plasma5Packages.plasma-workspace}/share/wayland-sessions";
+
+  # KDE Plasma
+  services.xserver.displayManager.defaultSession = "plasmawayland";
+  services.xserver.desktopManager.plasma5.enable = true;
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    gwenview
+    okular
+    oxygen
+    khelpcenter
+    konsole
+    plasma-browser-integration
+    print-manager
+  ];
+  
   virtualisation.vmVariant = {
     # nixos-rebuild build-vm --flake .#mini
     # following configuration is added only when building VM with build-vm
