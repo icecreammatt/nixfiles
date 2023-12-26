cbcc06acb0fb3006eeb3ced0eb24d7b554271cf8
Matt Carrier <icecreammatt@gmail.com>
2023-12-26 14:54:56 -0800

    fix: turn off gdm wayland

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -188,7 +188,7 @@ in
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
+  # services.xserver.displayManager.gdm.wayland = true;
   #services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.displayManager.sddm.enable = true;
