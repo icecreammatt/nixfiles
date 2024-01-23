05fa00874fdce85c8740c725f6c33910c6291082
Matt Carrier <icecreammatt@gmail.com>
2024-01-23 00:59:01 -0800

    feat: add kopia to mini

--- hosts/nixos/mini/configuration.nix
+++ hosts/nixos/mini/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./caddy.nix
+      ./kopia.nix
       ./logging.nix
     ];
 
---
+++ hosts/nixos/mini/kopia.nix
@@ -0,0 +1,33 @@
+{ pkgs, config, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    kopia
+  ];
+
+ sops.secrets."kopia/PASSWORD" = {
+    sopsFile = ../../../.secrets/kopia.ini;
+    format = "ini";
+  };
+
+  sops.secrets."kopia/USERNAME" = {
+    sopsFile = ../../../.secrets/kopia.ini;
+    format = "ini";
+  };
+
+  systemd.services.kopia = {
+    enable = true;
+    description = "kopia";
+    serviceConfig = {
+      ExecStart = "${pkgs.kopia}/bin/kopia server start --insecure --server-username $USERNAME --server-password $PASSWORD";
+      Type = "simple";
+      EnvironmentFile = [
+        config.sops.secrets."kopia/PASSWORD".path
+        config.sops.secrets."kopia/USERNAME".path
+      ];
+      Restart = "always";
+      RestartSec=1;
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+}
