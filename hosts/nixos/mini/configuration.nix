9fc75be90631394ede01393ff3d696bd14cef80f
Matt Carrier <icecreammatt@gmail.com>
2024-02-03 13:20:09 -0800

    feat: add backup script to stage files for kopia

--- hosts/nixos/mini/configuration.nix
+++ hosts/nixos/mini/configuration.nix
@@ -120,6 +120,28 @@
     };
   };
 
+  systemd.timers."vaultwarden-backup" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      Persistent = true;
+      AccuracySec = "1min";
+      OnCalendar = "*-*-* 04:00:00"; # Run at 4:00 AM daily
+      Unit = "vaultwarden-backup.service";
+    };
+  };
+
+  systemd.services."vaultwarden-backup" = {
+    path = with pkgs; [sqlite];
+    script = ''
+      ${pkgs.sqlite}/bin/sqlite3 /var/lib/bitwarden_rs/db.sqlite3 ".backup '/mnt/storage/backup/vaultwarden/db-$(date '+%Y%m%d-%H%M').sqlite3'"
+    '';
+
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
+
   virtualisation.vmVariant = {
     # nixos-rebuild build-vm --flake .#mini
     # following configuration is added only when building VM with build-vm
