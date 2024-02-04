e75ab2f94dab78e52dd590b41879b5ef9065a497
Matt Carrier <icecreammatt@gmail.com>
2024-02-04 14:51:03 -0800

    fix: enable docker rootless

--- hosts/nixos/pi4/configuration.nix
+++ hosts/nixos/pi4/configuration.nix
@@ -59,6 +59,11 @@ in {
   '';
 
   virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
   virtualisation.oci-containers = {
     backend = "docker";
     containers = {
