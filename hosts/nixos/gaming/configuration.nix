c71ca288f478bbc5e79267ce8ba04c7c1c5781ff
Matt Carrier <icecreammatt@gmail.com>
2024-03-02 14:02:30 -0800

    fix: address config warning

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -211,9 +211,9 @@ in {
   ];
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
     # xkbVariant = "colemak_dh,";
   };
 
