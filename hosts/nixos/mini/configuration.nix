edc51bf5076dedb61ac947b4e8652032d8e09499
Matt Carrier <icecreammatt@gmail.com>
2024-03-21 23:56:36 -0700

    feat: enable keyboard dev on mini

--- hosts/nixos/mini/configuration.nix
+++ hosts/nixos/mini/configuration.nix
@@ -15,6 +15,7 @@
     ../../../modules/apps/vaultwarden.nix
     ../../../modules/apps/navidrone.nix
     ../../../modules/ci/hydra.nix
+    ../../../modules/keyboard-dev.nix
     ./caddy.nix
     ./kopia.nix
     ./logging.nix
