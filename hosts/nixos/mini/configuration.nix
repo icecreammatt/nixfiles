332a0dd86e1ca23f2d37cae3e01e0902b1e20ee7
Matt Carrier <icecreammatt@gmail.com>
2024-04-12 10:27:20 -0700

    feat: disable attic

--- hosts/nixos/mini/configuration.nix
+++ hosts/nixos/mini/configuration.nix
@@ -59,56 +59,56 @@ in {
   users.extraGroups.docker.members = [user];
   users.users.${user}.extraGroups = ["docker"];
 
-  sops.secrets."attic/ATTIC_SERVER_TOKEN_HS256_SECRET_BASE64" = {
-    sopsFile = ../../../.secrets/attic.ini;
-    format = "ini";
-  };
+  # sops.secrets."attic/ATTIC_SERVER_TOKEN_HS256_SECRET_BASE64" = {
+  #   sopsFile = ../../../.secrets/attic.ini;
+  #   format = "ini";
+  # };
 
   programs.keyboard-dev = {
     enable = false;
   };
 
-  services.atticd = {
-    enable = true;
-    # package = pkgs.attic;
+  # services.atticd = {
+  #   enable = true;
+  #   # package = pkgs.attic;
 
-    # Replace with absolute path to your credentials file
-    # credentialsFile = "/etc/atticd.env";
-    credentialsFile = config.sops.secrets."attic/ATTIC_SERVER_TOKEN_HS256_SECRET_BASE64".path; # "/path/to/my/secrets/file";
+  #   # Replace with absolute path to your credentials file
+  #   # credentialsFile = "/etc/atticd.env";
+  #   credentialsFile = config.sops.secrets."attic/ATTIC_SERVER_TOKEN_HS256_SECRET_BASE64".path; # "/path/to/my/secrets/file";
 
-    settings = {
-      listen = "127.0.0.1:8072";
+  #   settings = {
+  #     listen = "127.0.0.1:8072";
 
-      storage = {
-        type = "local";
-        path = "/tmp/attic";
-      };
+  #     storage = {
+  #       type = "local";
+  #       path = "/tmp/attic";
+  #     };
 
-      # Data chunking
-      #
-      # Warning: If you change any of the values here, it will be
-      # difficult to reuse existing chunks for newly-uploaded NARs
-      # since the cutpoints will be different. As a result, the
-      # deduplication ratio will suffer for a while after the change.
-      chunking = {
-        # The minimum NAR size to trigger chunking
-        #
-        # If 0, chunking is disabled entirely for newly-uploaded NARs.
-        # If 1, all NARs are chunked.
-        nar-size-threshold = 64 * 1024; # 64 KiB
+  #     # Data chunking
+  #     #
+  #     # Warning: If you change any of the values here, it will be
+  #     # difficult to reuse existing chunks for newly-uploaded NARs
+  #     # since the cutpoints will be different. As a result, the
+  #     # deduplication ratio will suffer for a while after the change.
+  #     chunking = {
+  #       # The minimum NAR size to trigger chunking
+  #       #
+  #       # If 0, chunking is disabled entirely for newly-uploaded NARs.
+  #       # If 1, all NARs are chunked.
+  #       nar-size-threshold = 64 * 1024; # 64 KiB
 
-        # The preferred minimum size of a chunk, in bytes
-        min-size = 16 * 1024; # 16 KiB
+  #       # The preferred minimum size of a chunk, in bytes
+  #       min-size = 16 * 1024; # 16 KiB
 
-        # The preferred average size of a chunk, in bytes
-        avg-size = 64 * 1024; # 64 KiB
+  #       # The preferred average size of a chunk, in bytes
+  #       avg-size = 64 * 1024; # 64 KiB
 
-        # The preferred maximum size of a chunk, in bytes
-        max-size = 256 * 1024; # 256 KiB
-      };
-    };
-  };
-  systemd.services.atticd.serviceConfig.ReadWritePaths = "/tmp/attic";
+  #       # The preferred maximum size of a chunk, in bytes
+  #       max-size = 256 * 1024; # 256 KiB
+  #     };
+  #   };
+  # };
+  # systemd.services.atticd.serviceConfig.ReadWritePaths = "/tmp/attic";
 
   # systemd.timers."gauge-check" = {
   #   wantedBy = ["timers.target"];
