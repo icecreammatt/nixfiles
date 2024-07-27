5475966a93c9edc3a2df254f285a2c05ed833449
Matt Carrier <icecreammatt@gmail.com>
2024-07-27 14:12:00 -0700

    feat: add java dev tools and load testing

--- hosts/darwin/hosts/Bebop/default.nix
+++ hosts/darwin/hosts/Bebop/default.nix
@@ -7,18 +7,25 @@
     ];
 
     home.packages = with pkgs; [
+      atac
       audacity
       automake
       awscli2
       avrdude
       doctl
+      drill
       hex2color
+      httpie
+      hoppscotch
+      jetbrains.idea-community
+      jdk22
       lilypond-with-fonts
       nodePackages.pnpm
       nodePackages.typescript-language-server
       nodejs-18_x
       opentofu
       pocketbase
+      redis
       wezterm
     ];
   };
