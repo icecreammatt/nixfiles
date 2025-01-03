c800c444d9b7b6feb50ed258eaef0424e7c056c1
Matt Carrier <icecreammatt@gmail.com>
2025-01-03 15:59:45 -0800

    feat: add vlc

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -421,6 +421,7 @@ in {
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     waybar
     waypipe
+    vlc
     obs-studio
 
     # Wine for Ableton (Using Steam Proton instead)
