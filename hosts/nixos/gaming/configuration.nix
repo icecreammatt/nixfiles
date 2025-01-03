0081a43ba0dbd60eef3db8140ad6035b13093180
Matt Carrier <icecreammatt@gmail.com>
2025-01-03 15:59:16 -0800

    feat: add prusa slicer

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -410,6 +410,7 @@ in {
     #neovim
     nodejs
     sops
+    prusa-slicer
     sublime-merge
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     waybar
