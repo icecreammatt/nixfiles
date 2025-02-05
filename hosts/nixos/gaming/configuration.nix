c14e02e1fe66713d26eba2ec0aefd82889a9300d
Matt Carrier <icecreammatt@gmail.com>
2025-02-05 00:55:20 -0800

    docs: pin values for current driver

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -109,6 +109,14 @@ in {
       package = config.boot.kernelPackages.nvidiaPackages.beta;
 
       # persistencedSha256 = lib.fakeSha256;
+      #
+      # nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      #     version = "570.86.16";
+      #     sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
+      #     openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
+      #     settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
+      #     persistencedSha256 = "sha256-3mp9X/oV8o2TH9720NnoXROxQ4g98nNee+DucXpQy3w=";
+      # };
 
       # Doesn't boot with kde plasma6 on 6.11.5 kernel version
       # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
