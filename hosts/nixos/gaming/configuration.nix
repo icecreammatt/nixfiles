8db36398a57f88a44c9dddc15ab6fba5fe905807
Matt Carrier <icecreammatt@gmail.com>
2025-02-05 00:55:30 -0800

    fix: update pulse audio to service value

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -379,7 +379,7 @@ in {
 
   # Enable sound with pipewire.
   # sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
