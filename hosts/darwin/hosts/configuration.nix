233d981c23762e9ba8fdda06da45551ba9c45ec8
Matt Carrier <icecreammatt@gmail.com>
2025-03-27 16:46:23 -0700

    fix: update touchIdAuth setting

--- hosts/darwin/hosts/configuration.nix
+++ hosts/darwin/hosts/configuration.nix
@@ -34,7 +34,7 @@
   ];
 
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
