955c85260f2681d4ba1fffb61c192d5be7cd3380
Matt Carrier <icecreammatt@gmail.com>
2025-06-01 11:06:36 -0700

    feat: use flatpak slicer for now nvidia drivers broken with nix

--- hosts/nixos/gaming/configuration.nix
+++ hosts/nixos/gaming/configuration.nix
@@ -467,7 +467,7 @@ in {
     #neovim
     nodejs
     sops
-    prusa-slicer
+    # prusa-slicer
     sublime-merge
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # waybar
