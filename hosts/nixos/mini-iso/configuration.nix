da0d4b20d2023fb037b5ab3dda7eb6db14a98673
Matt Carrier <icecreammatt@gmail.com>
2025-09-01 12:18:13 -0700

    feat: update iso config

--- hosts/nixos/mini-iso/configuration.nix
+++ hosts/nixos/mini-iso/configuration.nix
@@ -30,6 +30,7 @@ in {
     users."${user}" = {
       imports = [
         ../../../modules/common.nix
+        ../../../modules/core.nix
       ];
     };
   };
@@ -66,6 +67,7 @@ in {
     nil
     tmux
     wezterm
+    neovim
     yazi
   ];
 
@@ -93,5 +95,6 @@ in {
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  # system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
